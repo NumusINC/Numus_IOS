5b7100addf4f16decec6995e257834d0ec9a1a07
Arturo Amador Paulino <amadorassassin@gmail.com>
2018-10-18 14:39:14 -0500

    Distribución de proyecto, y requeridos frameworks

--- Numus/Controller/AppDelegate.swift
+++ Numus/Controller/AppDelegate.swift
@@ -17,7 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Init configuration of firebase
         FirebaseApp.configure()
+        
+        //Create a reference to our database and connect
+        let dataBase = Database.database().reference()
+        
+        dataBase.setValue("hola firebase")
+        
         return true
     }
 
--- Podfile
+++ Podfile
@@ -9,5 +9,17 @@ target 'Numus' do
   pod 'SwiftyJSON'
   pod 'Alamofire'
   pod 'SVProgressHUD'
+  pod 'FirebaseUI/Auth'
+  pod 'FirebaseUI/Google'
+  pod 'FirebaseUI/Facebook'
+  pod 'Firebase/Database'
 
+end
+
+post_install do |installer|
+    installer.pods_project.targets.each do |target|
+        target.build_configurations.each do |config|
+            config.build_settings['CLANG_WARN_DOCUMENTATION_COMMENTS'] = 'NO'
+        end
+    end
 end
